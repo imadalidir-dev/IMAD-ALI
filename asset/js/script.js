e3178733b176aee6fad1c05a0f49f4a438c426d1
imadalidir-dev <lewanylwany@gmail.com>
2025-10-05 05:59:33 +0500

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,21 @@
+// Menu toggle for mobile
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// ScrollReveal animations
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 1500,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .hobbies-container, .project-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
